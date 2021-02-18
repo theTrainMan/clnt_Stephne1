8d89f86a23315190d7b24094a89eb8d00805e711
theTrainMan <stormbreaker70@gmail.com>
2021-02-18 11:42:15 +0200

    Home Page and Contact Page
    1. Added the Navbar and styling
    2. Added the Features and styling
    3. Added Contact Information and Styling

---
+++ main.js
@@ -0,0 +1,12 @@
+//JQuery to stretch the header across the screen
+$(document).ready(function() {
+    $('.header').height($(window).height());
+})
+
+var ready = (callback) => {
+    if (document.readyState != 'loading') callback();
+    else document.addEventListener('DOMContentLoaded', callback);
+}
+ready(() => {
+    document.querySelector('.header').getElementsByClassName.height = window.innerHeight + 'px';
+})
